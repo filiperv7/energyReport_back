46b074f41f1f85e4e2c6791254db01ae226810a6
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-10-21 00:43:00 -0300

    🚀 deploy config

--- src/Server.ts
+++ src/Server.ts
@@ -16,7 +16,8 @@ registerRoutes(app)
 
 app.listen(
   {
-    port: Number(process.env.PORT) || 3100
+    port: Number(process.env.PORT) || 3100,
+    host: '0.0.0.0'
   },
   () => console.log(`Server is running on port ${process.env.PORT || 3100}`)
 )
